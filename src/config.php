e1595db5831d806946a887fffee5528767cf7a13
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-12-10 21:12:03 +0530

    Stop tracking src and .htaccess

--- src/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Error handling settings
-ini_set('display_errors', 0);
-error_reporting(0);
-
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/errors.log');  // Project folder me file ban jayegi
-?>
